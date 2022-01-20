08d3365903f763d33e439c885f0cbe1edafd26d2
Wisarud2k <59276807+Wisarud2k@users.noreply.github.com>
2022-01-20 21:05:14 +0700

    fix overflow

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -24,6 +24,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: CustomAppBar(),
       body: BlocBuilder<SwipeBloc, SwipeState>(
         builder: (context, state) {
@@ -35,87 +36,33 @@ class HomeScreen extends StatelessWidget {
             );
           } else if (state is SwipeLoaded) {
             // swipe loaded state
-            return Column(
-              children: [
-                InkWell(
-                  onDoubleTap: () {
-                    Navigator.pushNamed(context, '/users',
-                        arguments: state.users[0]);
-                  },
-                  child: Draggable<User>(
-                    data: state.users[0],
-                    child: UserCard(user: state.users[0]),
-                    feedback: UserCard(user: state.users[0]),
-                    childWhenDragging: UserCard(user: state.users[1]),
-                    onDragEnd: (drag) {
-                      if (drag.velocity.pixelsPerSecond.dx < 0) {
-                        context.read<SwipeBloc>()
-                          ..add(SwipeLeftEvent(user: state.users[0]));
-                        print("swipe left");
-                      } else {
-                        context.read<SwipeBloc>()
-                          ..add(SwipeRightEvent(user: state.users[0]));
-                        print('Swipe Right');
-                      }
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  InkWell(
+                    onDoubleTap: () {
+                      Navigator.pushNamed(context, '/users',
+                          arguments: state.users[0]);
                     },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 6,
-                    horizontal: 60,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      InkWell(
-                        // button
-                        onTap: () {
-                          // cross button (X)
+                    child: Draggable<User>(
+                      data: state.users[0],
+                      child: UserCard(user: state.users[0]),
+                      feedback: UserCard(user: state.users[0]),
+                      childWhenDragging: UserCard(user: state.users[1]),
+                      onDragEnd: (drag) {
+                        if (drag.velocity.pixelsPerSecond.dx < 0) {
                           context.read<SwipeBloc>()
                             ..add(SwipeLeftEvent(user: state.users[0]));
                           print("swipe left");
-                        },
-                        child: ChoiceButton(
-                            width: 60,
-                            height: 60,
-                            hasGradient: false,
-                            color: Colors.white,
-                            icon: Icons.clear_rounded,
-                            size: 25),
-                      ),
-                      InkWell(
-                        // like button
-                        onTap: () {
+                        } else {
                           context.read<SwipeBloc>()
                             ..add(SwipeRightEvent(user: state.users[0]));
-                          print("swipe Right");
-                        },
-                        child: ChoiceButton(
-                            width: 74,
-                            height: 74,
-                            hasGradient: true,
-                            color: Colors.white,
-                            icon: Icons.favorite,
-                            size: 30),
-                      ),
-                    ],
+                          print('Swipe Right');
+                        }
+                      },
+                    ),
                   ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withAlpha(90),
-                        spreadRadius: 3,
-                      ),
-                    ],
-                  ),
-                  height: 50,
-                  width: MediaQuery.of(context).size.width / 1.2,
-                  child: Padding(
+                  Padding(
                     padding: const EdgeInsets.symmetric(
                       vertical: 6,
                       horizontal: 60,
@@ -124,39 +71,95 @@ class HomeScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
                         InkWell(
-                          // home
+                          // button
                           onTap: () {
                             // cross button (X)
-                            Navigator.pushNamed(context, "/");
-                            print("homepage !");
+                            context.read<SwipeBloc>()
+                              ..add(SwipeLeftEvent(user: state.users[0]));
+                            print("swipe left");
                           },
-                          child: Menubutton(
-                              width: 50,
-                              height: 50,
+                          child: ChoiceButton(
+                              width: 60,
+                              height: 60,
                               hasGradient: false,
-                              color: Colors.black,
-                              icon: Icons.home,
+                              color: Colors.white,
+                              icon: Icons.clear_rounded,
                               size: 25),
                         ),
                         InkWell(
-                          // home
+                          // like button
                           onTap: () {
-                            Navigator.pushNamed(context, "/messagebox");
-                            print("Message box !");
+                            context.read<SwipeBloc>()
+                              ..add(SwipeRightEvent(user: state.users[0]));
+                            print("swipe Right");
                           },
-                          child: Menubutton(
-                              width: 50,
-                              height: 50,
-                              hasGradient: false,
-                              color: Colors.black,
-                              icon: Icons.messenger_sharp,
+                          child: ChoiceButton(
+                              width: 74,
+                              height: 74,
+                              hasGradient: true,
+                              color: Colors.white,
+                              icon: Icons.favorite,
                               size: 30),
                         ),
                       ],
                     ),
                   ),
-                ),
-              ],
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withAlpha(90),
+                          spreadRadius: 3,
+                        ),
+                      ],
+                    ),
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 6,
+                        horizontal: 60,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          InkWell(
+                            // home
+                            onTap: () {
+                              // cross button (X)
+                              Navigator.pushNamed(context, "/");
+                              print("homepage !");
+                            },
+                            child: Menubutton(
+                                width: 50,
+                                height: 50,
+                                hasGradient: false,
+                                color: Colors.black,
+                                icon: Icons.home,
+                                size: 25),
+                          ),
+                          InkWell(
+                            // home
+                            onTap: () {
+                              Navigator.pushNamed(context, "/messagebox");
+                              print("Message box !");
+                            },
+                            child: Menubutton(
+                                width: 50,
+                                height: 50,
+                                hasGradient: false,
+                                color: Colors.black,
+                                icon: Icons.messenger_sharp,
+                                size: 30),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             );
           } else {
             return Text("Something Went Wrong");
