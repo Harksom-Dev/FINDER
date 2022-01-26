969b47459b0e8c5ca409b1949a53aa9d7a8cda2e
ponefire1337 <charttrakarn6@hotmail.co.th>
2022-01-26 23:37:21 +0700

    init web plus fix missing default firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:loginsystem/helper/helperfunction.dart';
 import 'package:loginsystem/screens/messagebox/chatroom_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
   runApp(MyApp());
 }
 
@@ -30,7 +30,10 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    Firebase.initializeApp();
+    WidgetsFlutterBinding.ensureInitialized();
+    Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     getLoggedInState();
     super.initState();
   }
